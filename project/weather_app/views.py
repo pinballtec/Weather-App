a36185af040626f1b7b7d7bbdff8bc8788fd1e41
pinballtec <daniilboiko99@gmail.com>
2022-04-09 22:50:21 +0200

    open weather is connected

--- project/weather_app/views.py
+++ project/weather_app/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+import requests
 from django.http import HttpResponse
 # Create your views here.
 
 
 def main(request):
+    app_id = ''
+
+    context = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=' + app_id
+
+    city = 'Warsaw'
+
+    resp = requests.get(context.format(city))
+    print(resp.text)
+
     return render(request, 'weather_app/index.html')
