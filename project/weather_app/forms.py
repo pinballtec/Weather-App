4d614854d2b29a1af3fa2ade6dc1d3378790e90c
pinballtec <daniilboiko99@gmail.com>
2022-04-12 14:22:17 +0200

    search bar is finshed

--- project/weather_app/forms.py
+++ project/weather_app/forms.py
@@ -6,5 +6,10 @@ class CityForm(ModelForm):
     class Meta:
         model = City
         fields = ['name']
-
+        widgets = {'name': TextInput(attrs={
+            'class': 'form-control',
+            'name': 'city',
+            'id': 'city',
+            'placeholder': 'Your city'
+        })}
 
